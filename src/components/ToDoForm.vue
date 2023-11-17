ecd3ad5c2eb8fa64d1b8be370233882cf4347488
pkbranca <sebastian.brancacho@gmail.com>
2023-11-16 23:48:47 -0500

    form validation

--- src/components/ToDoForm.vue
+++ src/components/ToDoForm.vue
@@ -1,30 +1,31 @@
 <template>
   <div>
  
-    <div class="mb-6">
-      <label for="newTask" class="text-left block mb-2 text-sm font-medium text-gray-900 dark:text-white">New task</label>
-      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700
-               leading-tight focus:outline-none focus:shadow-outline"
-               id="username" type="text"
-               v-model.lazy="localValue.name"
-               placeholder="Your task"> 
-    </div>
-    <div class="mb-6">
-      <label for="newTask" class="text-left block mb-2 text-sm font-medium text-gray-900 dark:text-white">Due date</label>
-      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700
-               leading-tight focus:outline-none focus:shadow-outline"
-               type="date" 
-               v-model.lazy="localValue.date"
-               placeholder="Due date">
-    </div>
-    <div>
-      <button type="submit" class="btn_submit text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-100 px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-      @click="addTask">
-        Submit
-      </button>
-    </div>
-    <!-- <form class="formTask m-auto" @submit="">
-  </form> -->
+    <form @submit="addTask">
+      <div class="mb-6">
+        <label for="newTask" class="text-left block mb-2 text-sm font-medium text-gray-900 dark:text-white">New task</label>
+        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700
+                 leading-tight focus:outline-none focus:shadow-outline"
+                 id="username" type="text"
+                 required
+                 v-model.lazy="localValue.name"
+                 placeholder="Your task">
+      </div>
+      <div class="mb-6">
+        <label for="newTask" class="text-left block mb-2 text-sm font-medium text-gray-900 dark:text-white">Due date</label>
+        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700
+                 leading-tight focus:outline-none focus:shadow-outline"
+                 type="date"
+                 required
+                 v-model.lazy="localValue.date"
+                 placeholder="Due date">
+      </div>
+      <div>
+        <button type="submit" class="btn_submit text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-100 px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+          Submit
+        </button>
+      </div>
+    </form>
   </div>
 
 </template>
@@ -56,7 +57,8 @@ export default {
     };
   },
   methods: {
-    addTask(){
+    addTask(e){
+      e.preventDefault();
       this.$emit('addTask', { task: this.localValue});
     }
   }
